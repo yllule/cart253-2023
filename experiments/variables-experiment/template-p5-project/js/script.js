564176acb0acf86b500756c7429075d52a74805e
Catherine Z <catherinezaloshnja@gmail.com>
2023-09-19 22:57:45 -0400

    Learned about changing variables

--- experiments/variables-experiment/template-p5-project/js/script.js
+++ experiments/variables-experiment/template-p5-project/js/script.js
@@ -6,9 +6,11 @@
 "use strict";
 
 let backgroundShade = 0;
-let circleX = 250;
+let circleX = 0;
 let circleY = 250;
 let circleSize = 100;
+let circleSpeed = 2;
+let circleAcceleration = 0.25;
 
 /**
  * Description of preload
@@ -31,6 +33,8 @@ function setup() {
 */
 function draw() {
     background(backgroundShade);
+    circleX += circleSpeed;
+    circleSpeed += circleAcceleration;
     ellipse(circleX, circleY, circleSize);
 
 }
