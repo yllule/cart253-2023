0fdea661dc9c51e2c92f9e56903c5c8d799073b5
Catherine Z <catherinezaloshnja@gmail.com>
2023-09-20 22:39:19 -0400

    A3 : Completed the activity

--- activities/03-moving-pictures/template-p5-project/js/script.js
+++ activities/03-moving-pictures/template-p5-project/js/script.js
@@ -1,10 +1,34 @@
 /**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Activity 3 - Moving Pictures
+ * Catherine
  */
+
+
+let bg = {
+    r: 0,
+    g: 0,
+    b: 0
+};
+
+let circle1 = {
+    x: 0,
+    y: 250,
+    size: 100,
+    growthRate: 1,
+    speed: 1,
+    fill: 255,
+    alpha: 200
+};
+
+let circle2 = {
+    x: 500,
+    y: 250,
+    size: 75,
+    sizeRatio: 0.75,
+    speed: 1,
+    fill: 255,
+    alpha: 200
+};
 
 "use strict";
 
@@ -20,7 +44,8 @@ function preload() {
  * Description of setup
 */
 function setup() {
-
+    createCanvas(500, 500);
+    noStroke();
 }
 
 
@@ -28,5 +53,25 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+
+    //makes bg turn redder
+    background(bg.r, bg.g, bg.b);
+    bg.r = map(circle1.size, 100, width, 0, 255);
+
+    //left circle
+    circle1.x = circle1.x + circle1.speed;
+    circle1.x = constrain(circle1.x, 0, width/2);
+    circle1.size = circle1.size + circle1.growthRate;
+    circle1.size = constrain(circle1.size, 0, width);
+    fill(circle1.fill, circle1.alpha);
+    ellipse(circle1.x, circle1.y, circle1.size);
+
+    //right circle
+    circle2.x = circle2.x + circle2.speed;
+    circle2.x = constrain(circle2.x, width/2, width);
+    circle2.size = circle1.size * circle2.sizeRatio;
+    fill(circle2.fill, circle2.alpha);
+    ellipse(circle2.x, circle2.y, circle2.size);
+
 
 }
