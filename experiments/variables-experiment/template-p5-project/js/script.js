08866a817274c66043a4f182da8aa178a7083b6d
Catherine Z <catherinezaloshnja@gmail.com>
2023-09-20 21:49:53 -0400

    Maps and constraints

--- experiments/variables-experiment/template-p5-project/js/script.js
+++ experiments/variables-experiment/template-p5-project/js/script.js
@@ -15,7 +15,8 @@ let circle = {
     x: 0,
     y: 250,
     size: 100,
-    speed: 2
+    speed: 2,
+    fill: 255
 };
 
 /**
@@ -39,7 +40,19 @@ function setup() {
 */
 function draw() {
     background(backgroundShade);
+
     circle.x = circle.x + circle.speed;
+
+    //circle.speed = random(-5, 5);
+    //circle.fill = random(0,255);
+
+    circle.x = constrain(circle.x, 0, width);
+
+    circle.size = map(mouseY, 0 ,height, 50, 500);
+
+    circle.fill = map(mouseX, 0, width, 0, 255);
+    fill(circle.fill);
     ellipse(circle.x, circle.y, circle.size);
+
 
 }
