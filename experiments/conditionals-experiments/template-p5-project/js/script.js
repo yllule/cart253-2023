78dcf866e45c0ac6035c543186cb8ca4afd0e643
Catherine Z <catherinezaloshnja@gmail.com>
2023-09-26 22:40:49 -0400

    Mouse input video

--- experiments/conditionals-experiments/template-p5-project/js/script.js
+++ experiments/conditionals-experiments/template-p5-project/js/script.js
@@ -9,16 +9,16 @@
 "use strict";
 
 let circle = {
-    x: undefined,
-    y: undefined,
+    x: 250,
+    y: 250,
     size : 100
 };
 
-let dangerZone = {
-    x: 250,
-    y: 250,
-    size: 150
-};
+let bg = {
+    r: 0,
+    g: 0,
+    b: 0
+}
 
 /**
  * Description of preload
@@ -34,16 +34,6 @@ function preload() {
 function setup() {
     createCanvas(500,500);
 
-    circle.x = random (0, width);
-    circle.y = random (0, height);
-
-    let d = dist(circle.x, circle.y, dangerZone.x, dangerZone.y);
-    while (d < circle.size/2 + dangerZone.size/2) {
-        circle.x = random(0, width);
-        circle.y = random(0, height);
-        d = dist(circle.x, circle.y, dangerZone.x, dangerZone.y);
-    }
-
 }
 
 /**
@@ -52,15 +42,17 @@ function setup() {
 
 
 function draw() {
-    background(0);
+    background(bg.r, bg.g, bg.b);
 
-    //danger zone
-    noFill();
-    stroke(255, 0, 0);
-    ellipse(dangerZone.x, dangerZone.y, dangerZone.size);
-
-    fill(255);
-    noStroke();
     ellipse(circle.x, circle.y, circle.size);
     
+}
+
+function mouseWheel() {
+    circle.x = mouseX;
+    circle.y = mouseY;
+
+    bg.r = random(0,255);
+    bg.g = random(0,255);
+    bg.b = random(0,255);
 }
