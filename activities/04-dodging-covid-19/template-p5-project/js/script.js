8d1d5bf1c04645b428acf41a925fcc65ed996330
Catherine Z <catherinezaloshnja@gmail.com>
2023-09-26 23:53:28 -0400

    Dodging Covid19 activity done

--- activities/04-dodging-covid-19/template-p5-project/js/script.js
+++ activities/04-dodging-covid-19/template-p5-project/js/script.js
@@ -1,12 +1,32 @@
 /**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Dodging Covid-19 activity
+ * Catherine Zaloshnja
  */
 
 "use strict";
+
+let covid19 = {
+    x: 0,
+    y: 250,
+    size: 100,
+    vx : 0,
+    vy: 0,
+    speed: 5,
+    fill: {
+        r:255,
+        g:0,
+        b:0,
+    }
+};
+
+let user = {
+    x: 250,
+    y: 250,
+    size: 100,
+    fill: 255
+};
+
+let numStatic = 500;
 
 /**
  * Description of preload
@@ -20,7 +40,12 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(windowWidth, windowHeight);
 
+    covid19.y = random(0, height);
+    covid19.vx = covid19.speed;
+
+    noCursor();
 }
 
 
@@ -28,5 +53,42 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
 
+    //Display static
+    for (let i = 0; i < numStatic; i++) {
+        let x = random(0, width);
+        let y = random(0, height);
+        stroke(255);
+        point(x, y);
+    }
+
+
+
+    //Covid19 movement
+    covid19.x = covid19.x + covid19.vx;
+    covid19.y = covid19.y + covid19.vy;
+
+    if(covid19.x > width) {
+        covid19.x = 0;
+        covid19.y = random(0, height);
+    }
+
+    //User movement
+    user.x = mouseX;
+    user.y = mouseY;
+
+    //Check for catching covid19
+    let d = dist(user.x, user.y, covid19.x, covid19.y);
+    if (d < covid19.size/2 + user.size/2) {
+        noLoop();
+    }
+
+    //Display covid19
+    fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+    ellipse(covid19.x, covid19.y, covid19.size);
+
+    //Display user
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
 }
