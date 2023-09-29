3d4d942a59ee95c368db022d8cf9179eb6f1f911
Catherine Z <catherinezaloshnja@gmail.com>
2023-09-28 21:37:14 -0400

    5.1 and 5.2 videos

--- experiments/functions-experiments/template-p5-project/js/script.js
+++ experiments/functions-experiments/template-p5-project/js/script.js
@@ -1,9 +1,6 @@
 /**
  * Title of Project
  * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
  */
 
 "use strict";
@@ -20,7 +17,7 @@ function preload() {
  * Description of setup
 */
 function setup() {
-
+    createCanvas(500, 500);
 }
 
 
@@ -28,5 +25,21 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
 
+    parallels(100, 100, 50);
+    parallels(50, 50, 100);
+    parallels(300, 400, 5);
+    parallels(250, 250, 15);
+    parallels(275, 275, 20);
+}
+
+function parallels(x, y, numLines) {
+    for (let i = 0; i < numLines; i++) {
+        noStroke();
+        fill(255);
+        rectMode(CENTER);
+        rect(x, y, 2, 150);
+        x = x + 10;
+    }
 }
