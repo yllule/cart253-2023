845f50acbf6abd0da4c342b44eaf524cfd8ee135
Catherine Z <catherinezaloshnja@gmail.com>
2023-10-03 22:06:03 -0400

    Watched 5.5 video on states, experimentations

--- experiments/functions-experiments/template-p5-project/js/script.js
+++ experiments/functions-experiments/template-p5-project/js/script.js
@@ -5,6 +5,16 @@
 
 "use strict";
 
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 2
+};
+
+let state = `title`; // Possible states are : title, animation, ending
 
 /**
  * Description of preload
@@ -19,7 +29,9 @@ function preload() {
 */
 function setup() {
     createCanvas(500, 500);
-
+    circle.vx = circle.speed;
+    textSize(32);
+    textAlign(CENTER, CENTER);
 }
 
 
@@ -27,13 +39,47 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(127);
+    background(0);
 
-    textStyle(BOLD);
-    textSize(200);
-    textAlign(CENTER, CENTER);
-    fill(100, 40, 200);
-    stroke(50, 200, 200);
-    strokeWeight(3);
-    text(`Hello, world!`, 250, 250);
+    if (state === `title`) {
+        title();
+
+    }
+    else if (state === `animation`) {
+        animation();
+
+    }
+    else if (state === `ending`) {
+        ending();
+    }
+}
+
+function title() {
+    //title
+    fill(255);
+    text(`Life`, width/2, height/2);
+}
+
+function animation() {
+    //animation
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+    
+    if (circle.x > width) {
+        state = `ending`;
+    }
+
+    ellipse(circle.x, circle.y, circle.size);
+}
+
+function ending() {
+    //ending
+    fill(127);
+    text(`It's all over.`, width/2, height/2);
+}
+
+function keyPressed() {
+    if (state === `title`) {
+    state = `animation`
+    }
 }
