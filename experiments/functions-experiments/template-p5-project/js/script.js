59c00c0ed68a71362a4cf14deb4d318ad4c6a155
Catherine Z <catherinezaloshnja@gmail.com>
2023-10-03 22:28:33 -0400

    5.6 Keyboard input experimentations

--- experiments/functions-experiments/template-p5-project/js/script.js
+++ experiments/functions-experiments/template-p5-project/js/script.js
@@ -5,16 +5,7 @@
 
 "use strict";
 
-let circle = {
-    x: 0,
-    y: 250,
-    size: 100,
-    vx: 0,
-    vy: 0,
-    speed: 2
-};
-
-let state = `title`; // Possible states are : title, animation, ending
+let bg = 0;
 
 /**
  * Description of preload
@@ -29,9 +20,6 @@ function preload() {
 */
 function setup() {
     createCanvas(500, 500);
-    circle.vx = circle.speed;
-    textSize(32);
-    textAlign(CENTER, CENTER);
 }
 
 
@@ -39,47 +27,10 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(0);
+    background(bg);
 
-    if (state === `title`) {
-        title();
-
-    }
-    else if (state === `animation`) {
-        animation();
-
-    }
-    else if (state === `ending`) {
-        ending();
-    }
-}
-
-function title() {
-    //title
-    fill(255);
-    text(`Life`, width/2, height/2);
-}
-
-function animation() {
-    //animation
-    circle.x = circle.x + circle.vx;
-    circle.y = circle.y + circle.vy;
-    
-    if (circle.x > width) {
-        state = `ending`;
-    }
-
-    ellipse(circle.x, circle.y, circle.size);
-}
-
-function ending() {
-    //ending
-    fill(127);
-    text(`It's all over.`, width/2, height/2);
-}
-
-function keyPressed() {
-    if (state === `title`) {
-    state = `animation`
+    if (keyIsDown(65)) {
+        rectMode(CENTER);
+        rect(250, 250, 100);
     }
 }
