d5f7972d0aed1f91873acdd454ff292e7931d079
Catherine Z <catherinezaloshnja@gmail.com>
2023-10-12 01:26:54 -0400

    Removed the inventory box test
    Since I won't be using it for this project, my idea was too complex so while not much coding got done here, I was simplifying my idea

--- project_1/template-p5-project/js/script.js
+++ project_1/template-p5-project/js/script.js
@@ -1,19 +1,12 @@
 /**
  * Fishing simulator
  * Catherine Zaloshnja
+ * Originally I wanted the fishing game to be more complex and inspired by the fishing system in Final Fantasy XIV,
+ * but finally I settled for something simpler, more similar to Stardew Valley/Animal Crossing like fishing since it was more simpler to make.
  */
 
 "use strict";
 
-let inventoryBox = {
-    x: undefined,
-    y: undefined,
-    size: 200,
-    offsetX: 0,
-    offsetY: 0,
-    isBeingDragged: false,
-    feedbackSizeChangeAmount : 5
-};
 
 /**
  * Description of preload
@@ -27,9 +20,7 @@ function preload() {
  * Description of setup
 */
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    inventoryBox.x = width / 2;
-    inventoryBox.y = height / 2;
+  createCanvas(windowWidth, windowHeight);
 
 }
 
@@ -40,58 +31,4 @@ function setup() {
 function draw() {
     background(0);
 
-    handleDragging();
-    drawInventoryBox();
-}
-
-function handleDragging() {
-    if (inventoryBox.isBeingDragged) {
-      inventoryBox.x = mouseX + inventoryBox.offsetX;
-      inventoryBox.y = mouseY + inventoryBox.offsetY;
-  
-      inventoryBox.x = constrain(inventoryBox.x, 0, width);
-      inventoryBox.y = constrain(inventoryBox.y, 0, height);
-    }
-  }
-
-  function drawInventoryBox() {
-    push();
-    fill(255, 0, 0);
-    noStroke();
-    rect(inventoryBox.x, inventoryBox.y, inventoryBox.size);
-    pop();
-  }
-
-  function mouseIsInsideInventoryBox() {
-    let d = dist(mouseX, mouseY, inventoryBox.x, inventoryBox.y);
-    if (d < inventoryBox.size / 2) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  function mousePressed() {
-    // Only respond to the mouse click if the shape is active
-    // and the mouse is in the shape
-    if (mouseIsInsideInventoryBox()) {
-      // Start dragging
-      inventoryBox.isBeingDragged = true;
-      // Remember the offset of the shape's centre relative to the mouse
-      inventoryBox.offsetX = inventoryBox.x - mouseX;
-      inventoryBox.offsetY = inventoryBox.y - mouseY;
-      // Make the shape a bit smaller for feedback
-      inventoryBox.size -= inventoryBox.feedbackSizeChangeAmount;
-    }
-  }
-
-  function mouseReleased() {
-    // If the shape is currently being dragged
-    // and they've "dropped it" on the right side of the line
-    if (inventoryBox.isBeingDragged = false) {
-      // Then deactive the shape (it vanishes)
-      shape.size += shape.feedbackSizeChangeAmount;
-      shape.offsetX = 0;
-      shape.offsetY = 0;
-  }
 }
