1b524fc85a90f59d38cc47413b56430e7228b9df
yllule <catherinezaloshnja@gmail.com>
2023-10-15 17:06:01 -0400

    Starting over
    I changed the way I want to make the game work so that it is simpler, this means I got rid of some of the code I won't be needing in this newer version

--- project_1/template-p5-project/js/script.js
+++ project_1/template-p5-project/js/script.js
@@ -7,34 +7,6 @@
 
 "use strict";
 
-//setting the variable for the button to cast the line
-let buttonCast = {
-  x: undefined,
-  y: undefined,
-  width: 250,
-  height: 75,
-  //text position
-  textX: undefined,
-  textY: undefined,
-  isBeingPressed: false,
-  widthChangeAmount: 245,
-  heightChangeAmount: 70
-};
-
-//setting the variable for the button to hook the line
-let buttonHook = {
-  x: undefined,
-  y: undefined,
-  width: 250,
-  height: 75,
-  //text position
-  textX: undefined,
-  textY: undefined,
-  isBeingPressed: false,
-  widthChangeAmount: 245,
-  heightChangeAmount: 70
-};
-
 let inventoryBox = {
   x: undefined,
   y: undefined,
@@ -54,18 +26,7 @@ function preload() {
  * Description of setup
 */
 function setup() {
-  createCanvas(1500, 1000);
-  //setting up the position of the button + text, trying to center the text to the button by eye
-  buttonCast.x = width/5;
-  buttonCast.y = height-100;
-  buttonCast.textX = buttonCast.x;
-  buttonCast.textY = buttonCast.y+12;
-
-  //setting up the position of the second button so that i will always show next to the first one
-  buttonHook.x = buttonCast.x + 300;
-  buttonHook.y = buttonCast.y;
-  buttonHook.textX = buttonHook.x;
-  buttonHook.textY = buttonHook.y+12;
+  createCanvas(2000, 1000);
 
   //setting up the position of the inventory box
   inventoryBox.x = width-300;
@@ -79,75 +40,10 @@ function setup() {
 */
 function draw() {
   background(0);
-  drawButtonCast();
-  drawButtonHook();
   drawInventoryBox();
 
 }
 
-function drawButtonCast() {
-
-  //adding feedback to the button when it gets clicked
-  if (mouseIsPressed && mouseInsideButtonCast()) {
-    //display of cast button when clicked
-    push();
-    noStroke();
-    rectMode(CENTER);
-    rect(buttonCast.x, buttonCast.y, buttonCast.widthChangeAmount, buttonCast.heightChangeAmount, 25);
-    pop();
-    //you cast the line and the code will check which fish will bite
-    chooseFish();
-  } 
-  else {
-    //display of cast button when not clicked
-    push();
-    noStroke();
-    rectMode(CENTER);
-    rect(buttonCast.x, buttonCast.y, buttonCast.width, buttonCast.height, 25);
-    pop();
-  }
-
-//cast button text
-  push();
-  noStroke();
-  textSize(35);
-  textAlign(CENTER);
-  fill(255, 0, 0);
-  text('Cast line', buttonCast.textX, buttonCast.textY);
-  pop();
-}
-
-function drawButtonHook() {
-
-//adding feedback to the button when it gets clicked
-
-if (mouseIsPressed === true && mouseInsideButtonHook()) {
-  //display of hook button when clicked
-  push();
-  noStroke();
-  rectMode(CENTER);
-  rect(buttonHook.x, buttonHook.y, buttonHook.widthChangeAmount, buttonHook.heightChangeAmount, 25);
-  pop();
-  //once you click the button you reel in the fish
-  hookFish();
-} else {
-  //display of hook button when not clicked
-  push();
-  noStroke();
-  rectMode(CENTER);
-  rect(buttonHook.x, buttonHook.y, buttonHook.width, buttonHook.height, 25);
-  pop();
-}
-
-//hook button text
-  push();
-  noStroke();
-  textSize(35);
-  textAlign(CENTER);
-  fill(0);
-  text('Hook', buttonHook.textX, buttonHook.textY);
-  pop();
-}
 
 function drawInventoryBox(){
   push();
@@ -155,33 +51,6 @@ function drawInventoryBox(){
   rectMode(CENTER);
   rect(inventoryBox.x, inventoryBox.y, inventoryBox.width, inventoryBox.height, 50);
   pop();
-}
-
-function mouseInsideButtonCast() {
-  let d = dist(mouseX, mouseY, buttonCast.x, buttonCast.y);
-  if (mouseX >= buttonCast.x - buttonCast.width/2 && mouseX <= buttonCast.x + buttonCast.width/2 && mouseY >= buttonCast.y - buttonCast.height/2 && mouseY <= buttonCast.y + buttonCast.height/2) { //check if mouse is inside the cast button
-    return true;
-  } 
-  else {
-    return false;
-  }
-}
-
-function mouseInsideButtonHook() {
-  let d2 = dist(mouseX, mouseY, buttonHook.x, buttonHook.y);
-  if (mouseX >= buttonHook.x - buttonHook.width/2 && mouseX <= buttonHook.x + buttonHook.width/2 && mouseY >= buttonHook.y - buttonHook.height/2 && mouseY <= buttonHook.y + buttonHook.height/2) { //check if mouse is inside the hook button
-    return true;
-  } 
-  else {
-    return false;
-  }
-}
-
-function mouseClicked() {
-
-if(mouseInsideButtonCast===true) {
-  chooseFish();
-  }
 }
 
 function chooseFish() { //the code will decide which fish will bite, each fish/object has a drop rate % out of 100
@@ -251,9 +120,5 @@ function chooseFish() { //the code will decide which fish will bite, each fish/o
 }
 
 function perch() {
-
-}
-
-function hookFish() {
 
 }
