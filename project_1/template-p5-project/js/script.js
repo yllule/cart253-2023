e97b72d3ef684941b5c7729a5848599e32d863e2
yllule <catherinezaloshnja@gmail.com>
2023-10-17 13:03:56 -0400

    Added simple fish movements and tweaked visuals

--- project_1/template-p5-project/js/script.js
+++ project_1/template-p5-project/js/script.js
@@ -8,22 +8,24 @@
 
 "use strict";
 
-let state = 'title' //can be 'title', 'simulation'
+let state = 'title' //can be 'title', 'simulation', 'allFishCaught'
 
+//setting up the variables for all the image assets
 let borderImg;
 let fishBgImg;
 let hookImg;
 let fishShadowImg;
 let smallFishImg;
 
-
+//a box showing what fish the user has or has not yet caught
 let inventoryBox = {
   x: undefined,
   y: undefined,
-  width: 500,
+  width: 450,
   height: 900
 };
 
+//each different type of fish caught is worth a certain amount, this will keep track of the user's score
 let scoreBox = {
   x: undefined,
   y: undefined,
@@ -32,6 +34,7 @@ let scoreBox = {
   roundness: 20
 }
 
+//this box will keep count of how many fish the user has caught
 let fishCountBox = {
   x: undefined,
   y: undefined,
@@ -39,6 +42,17 @@ let fishCountBox = {
   height: 75,
   roundness: 20
 }
+
+let fish1 = {
+  x: 0,
+  y: 0,
+  //size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 2
+}
+
+
 
 /**
  * Description of preload
@@ -61,7 +75,7 @@ function setup() {
   createCanvas(2000, 1000);
 
   //setting up the position of the inventory box
-  inventoryBox.x = width-300;
+  inventoryBox.x = width-275;
   inventoryBox.y = height/2;
 
   //setting up the position of the score box
@@ -72,6 +86,12 @@ function setup() {
   fishCountBox.x = width-1250;
   fishCountBox.y = height-50;
 
+  //setting the fish1 x position to be at the left edge of the fishing interface box
+  fish1.x = 500;
+  //when the fish position resets, the y will be random and inside the fishing interface box
+  fish1.y = random(50, 950);
+  fish1.vx = fish1.speed;
+
 }
 
 
@@ -79,30 +99,53 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-  background(0);
+  background(100, 0, 220);
+  bgElements();
   display();
+  fish();
 
+}
 
+function bgElements() { // i made this function just to help with the layering of certain visual elements
+//these ones will be all the way in the back of the screen
+
+//main fishing box
+imageMode(CENTER);
+image(fishBgImg, width/2, height/2);
 
 }
 
 
 function display(){
 
+
+
+  //user display, it is just the tip of the hook
+  imageMode(CENTER);
+  ellipse(mouseX, mouseY, 25, 25);
+  //hook string, i want it to follow the mouse X position
+  push();
+  noStroke();
+  rectMode(CORNER);
+  //rect(mouseX, mouseY, 5, 1000);
+  pop();
+
+  //fish1 display
+  push();
+  imageMode(CENTER);
+  image(fishShadowImg, fish1.x, fish1.y);
+  pop();
+
   //fish inventory box
   push();
   noStroke();
   rectMode(CENTER);
-  rect(inventoryBox.x, inventoryBox.y, inventoryBox.width, inventoryBox.height, 50);
+  rect(inventoryBox.x, inventoryBox.y, inventoryBox.width, inventoryBox.height, 0, 50, 50, 0);
   pop();
 
   //border of the main fishing box
   imageMode(CENTER);
   image(borderImg, width/2, height/2);
-
-  //main fishing box
-  imageMode(CENTER);
-  image(fishBgImg, width/2, height/2);
 
   //score box
   push();
@@ -118,11 +161,27 @@ function display(){
   rect(fishCountBox.x, fishCountBox.y, fishCountBox.width, fishCountBox.height, fishCountBox.roundness);
   pop();
 
-  //user display, it is just the tip of the hook
-  imageMode(CENTER);
-  image(hookImg, mouseX, mouseY);
-  //hook string, i want it to follow the mouse X position
-  rect(mouseX, 0, 5, 1000);
+}
+
+function fish() {
+
+  //fish1 movement
+  fish1.x = fish1.x + fish1.vx;
+  fish1.y = fish1.y + fish1.vy;
+
+  //movement of the fish1
+  let swim = random();
+  if (swim < 0.03) {
+    fish1.vy = random(-fish1.speed, fish1.speed);
+  }
+
+  //fish1 will go offscreen a bit before resetting
+  let reset = 1800; //1500 = end of the fish interface + 300 to give it time to reset
+
+  if (fish1.x > reset) {
+    fish1.x = 500;
+    fish1.y = random(50, 950);
+  }
 
 }
 
@@ -134,59 +193,59 @@ function chooseFish() { //the code will decide which fish will bite, each fish/o
 		circle(100, 100, 50);
     //perch
 	}
-	if (i <= 22 && i > 11) {
+	else if (i <= 22 && i > 11) {
 		square(100, 100, 50);
     //bass
 	}
-  if (i <= 32 && i > 22) {
+  else if (i <= 32 && i > 22) {
     circle(150, 150, 75);
     //frog
   }
-  if (i <= 42 && i > 32){
+  else if (i <= 42 && i > 32){
     square(250, 250, 25);
     //loach
   }
-  if(i <= 52 && i > 42){
+  else if(i <= 52 && i > 42){
     circle(250, 100, 75);
     //carp
   }
-  if(i <= 61.5 && i > 52){
+  else if(i <= 61.5 && i > 52){
     square(200, 200, 25);
     //catfish
   }
-  if(i <= 69.5 && i > 61.5){
+  else if(i <= 69.5 && i > 61.5){
     circle(500, 500, 50);
     //empty soda can
   }
-  if(i <= 76.5 && i > 69.5){
+  else if(i <= 76.5 && i > 69.5){
     square(550, 550, 50);
     //soggy sock
   }
-  if(i <= 82.5 && i > 76.5){
+  else if(i <= 82.5 && i > 76.5){
     circle(300, 300, 75);
     //salmon
   }
-  if(i <= 88.5 && i > 82.5){
+  else if(i <= 88.5 && i > 82.5){
     square (300, 300, 100);
     //broken glasses
   }
-  if(i <= 93.5 && i > 88.5){
+  else if(i <= 93.5 && i > 88.5){
     circle(700, 700, 100);
     //koi
   }
-  if(i <= 97.5 && i > 93.5){
+  else if(i <= 97.5 && i > 93.5){
     square(750, 750, 50);
     //sturgeon
   }
-  if(i <= 98.5 && i > 97.5){
+  else if(i <= 98.5 && i > 97.5){
     circle(750, 750, 50);
     //crystal
   }
-  if(i <= 99.5 && i > 98.5){
+  else if(i <= 99.5 && i > 98.5){
     square(800, 800, 100);
     //treasure chest
   }
-  if(i <= 100 && i > 99.5){
+  else if(i <= 100 && i > 99.5){
     square(1000, 1000, 50);
     //mutant carp
   }
