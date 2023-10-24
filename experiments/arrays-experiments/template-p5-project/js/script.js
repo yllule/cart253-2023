eb3b8ddc6085dbbfdc1780c8cfaad45c79580411
yllule <catherinezaloshnja@gmail.com>
2023-10-24 12:27:08 -0400

    Started introduction to arrays

--- experiments/arrays-experiments/template-p5-project/js/script.js
+++ experiments/arrays-experiments/template-p5-project/js/script.js
@@ -1,12 +1,15 @@
 /**
- * Title of Project
- * Author Name
+ * Arrays experimentations
+ * Catherine Zaloshnja
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
  */
 
 "use strict";
+
+
+let school = [];
+let schoolSize = 10;
+
 
 /**
  * Description of preload
@@ -20,7 +23,26 @@ function preload() {
  * Description of setup
 */
 function setup() {
+createCanvas(600, 600);
 
+
+for (let i = 0; i < schoolSize; i++) {
+    let fish = createFish(random(0, width), random(0, height));
+    school.push(fish);
+//    school[i] = createFish(random(0, width), random(0, height));
+}
+}
+
+function createFish(x, y) {
+    let fish = {
+        x: x,
+        y: y,
+        size: 50,
+        vx: 0,
+        vy: 0,
+        speed: 2
+    };
+    return fish;
 }
 
 
@@ -28,5 +50,39 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
 
+    for (let i = 0; i < school.length; i++) {
+        moveFish(school[i]);
+        displayFish(school[i]);
+    }
+}
+
+function moveFish(fish) {
+
+    let change = random(0,1);
+    if (change < 0.05) {
+        fish.vx = random(-fish.speed, fish.speed);
+        fish.vy = random(-fish.speed, fish.speed);
+    }
+
+    fish.x = fish.x + fish.vx;
+    fish.y = fish.y + fish.vy;
+
+    fish.x = constrain(fish.x, 0, width);
+    fish.y = constrain(fish.y, 0, height);
+}
+
+function displayFish(fish) {
+
+    push();
+    fill(200, 100, 100);
+    noStroke();
+    ellipse(fish.x, fish.y, fish.size);
+    pop();
+}
+
+function mousePressed() {
+    let fish = createFish(mouseX, mouseY);
+    school.push(fish);
 }
