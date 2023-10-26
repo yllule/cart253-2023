6e5e8a584ea9c95ec590dbde7396d8f02e55cd72
Catherine Z <catherinezaloshnja@gmail.com>
2023-10-25 23:15:42 -0400

    Played around with the code to get ideas for the exercise

--- exercises/Age-of-aquariums-exercise/template-p5-project/js/script.js
+++ exercises/Age-of-aquariums-exercise/template-p5-project/js/script.js
@@ -6,49 +6,49 @@
 
 "use strict";
 
-let circle = {
-    x: 0,
-    y: 0,
-    size: 100,
-    trail: [],
-    trailSize: 20
-};
+// let circle = {
+//     x: 0,
+//     y: 0,
+//     size: 100,
+//     trail: [],
+//     trailSize: 20
+// };
 
-/**
- * Description of setup
-*/
-function setup() {
-    createCanvas(600,600);
-}
+// /**
+//  * Description of setup
+// */
+// function setup() {
+//     createCanvas(600,600);
+// }
 
-/**
- * Description of draw()
-*/
-function draw() {
+// /**
+//  * Description of draw()
+// */
+// function draw() {
 
-background(0);
+// background(0);
 
-circle.x = mouseX;
-circle.y = mouseY;
+// circle.x = mouseX;
+// circle.y = mouseY;
 
-for (let i = 0; i < circle.trail.length; i++) {
-    let position = circle.trail[i];
-    ellipse(position.x, position.y, circle.size);
-}
+// for (let i = 0; i < circle.trail.length; i++) {
+//     let position = circle.trail[i];
+//     ellipse(position.x, position.y, circle.size);
+// }
 
-ellipse(circle.x, circle.y, circle.size);
+// ellipse(circle.x, circle.y, circle.size);
 
-let newTrailPosition = {
-    x: circle.x,
-    y: circle.y,
-};
-circle.trail.push(newTrailPosition);
+// let newTrailPosition = {
+//     x: circle.x,
+//     y: circle.y,
+// };
+// circle.trail.push(newTrailPosition);
 
-if (circle.trail.length > circle.trailSize) {
-    circle.trail.shift();
-}
+// if (circle.trail.length > circle.trailSize) {
+//     circle.trail.shift();
+// }
 
-}
+// }
 
 //previous exercise
 
@@ -102,82 +102,82 @@ if (circle.trail.length > circle.trailSize) {
 
 //previous video exercise
 
-// let school = [];
-// let schoolSize = 10;
+let school = [];
+let schoolSize = 10;
 
 
-// /**
-//  * Description of preload
-// */
-// function preload() {
+/**
+ * Description of preload
+*/
+function preload() {
 
-// }
-
-
-// /**
-//  * Description of setup
-// */
-// function setup() {
-// createCanvas(600, 600);
+}
 
 
-// for (let i = 0; i < schoolSize; i++) {
-//     let fish = createFish(random(0, width), random(0, height));
-//     school.push(fish);
-// //    school[i] = createFish(random(0, width), random(0, height));
-// }
-// }
-
-// function createFish(x, y) {
-//     let fish = {
-//         x: x,
-//         y: y,
-//         size: 50,
-//         vx: 0,
-//         vy: 0,
-//         speed: 2
-//     };
-//     return fish;
-// }
+/**
+ * Description of setup
+*/
+function setup() {
+createCanvas(600, 600);
 
 
-// /**
-//  * Description of draw()
-// */
-// function draw() {
-//     background(0);
+for (let i = 0; i < schoolSize; i++) {
+    let fish = createFish(random(0, width), random(0, height));
+    school.push(fish);
+//    school[i] = createFish(random(0, width), random(0, height));
+}
+}
 
-//     for (let i = 0; i < school.length; i++) {
-//         moveFish(school[i]);
-//         displayFish(school[i]);
-//     }
-// }
+function createFish(x, y) {
+    let fish = {
+        x: x,
+        y: y,
+        size: 50,
+        vx: 0,
+        vy: 0,
+        speed: 2
+    };
+    return fish;
+}
 
-// function moveFish(fish) {
 
-//     let change = random(0,1);
-//     if (change < 0.05) {
-//         fish.vx = random(-fish.speed, fish.speed);
-//         fish.vy = random(-fish.speed, fish.speed);
-//     }
+/**
+ * Description of draw()
+*/
+function draw() {
+    background(0);
 
-//     fish.x = fish.x + fish.vx;
-//     fish.y = fish.y + fish.vy;
+    for (let i = 0; i < school.length; i++) {
+        moveFish(school[i]);
+        displayFish(school[i]);
+    }
+}
 
-//     fish.x = constrain(fish.x, 0, width);
-//     fish.y = constrain(fish.y, 0, height);
-// }
+function moveFish(fish) {
 
-// function displayFish(fish) {
+    let change = random(0,1);
+    if (change < 1) {
+        fish.vx = random(-fish.speed, fish.speed);
+        fish.vy = random(-fish.speed, fish.speed);
+    }
 
-//     push();
-//     fill(200, 100, 100);
-//     noStroke();
-//     ellipse(fish.x, fish.y, fish.size);
-//     pop();
-// }
+    fish.x = fish.x + fish.vx;
+    fish.y = fish.y + fish.vy;
 
-// function mousePressed() {
-//     let fish = createFish(mouseX, mouseY);
-//     school.push(fish);
-// }
+    fish.x = constrain(fish.x, 0, width);
+    fish.y = constrain(fish.y, 0, height);
+}
+
+function displayFish(fish) {
+
+    push();
+    fill(200, 100, 100);
+    noStroke();
+    ellipse(fish.x, fish.y, fish.size);
+    pop();
+}
+
+function mouseDragged() {
+    let fish = createFish(mouseX, mouseY);
+    school.push(fish);
+}
