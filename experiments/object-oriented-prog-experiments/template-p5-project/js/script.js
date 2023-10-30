e110dee632918b3b94ce6b5d1bf664f827521eb9
yllule <catherinezaloshnja@gmail.com>
2023-10-30 01:14:57 -0400

    Finished video 1 for object oriented programming

--- experiments/object-oriented-prog-experiments/template-p5-project/js/script.js
+++ experiments/object-oriented-prog-experiments/template-p5-project/js/script.js
@@ -1,12 +1,24 @@
 /**
- * Title of Project
- * Author Name
+ * Object Oriented Programming experiments
+ * Catherine Zaloshnja
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
  */
 
 "use strict";
+
+// Our garden
+let garden = {
+    // An array to store the individual flowers
+    flowers: [],
+    // How many flowers in the garden
+    numFlowers: 20,
+    // The color of the grass (background)
+    grassColor: {
+      r: 120,
+      g: 180,
+      b: 120
+    }
+  };
 
 /**
  * Description of preload
@@ -21,6 +33,15 @@ function preload() {
 */
 function setup() {
 
+createCanvas(600, 600);
+
+  // Create our flowers by counting up to the number of the flowers
+  for (let i = 0; i < garden.numFlowers; i++) {
+    // Create a new flower
+    let flower = new Flower();
+    // Add the flower to the array of flowers
+    garden.flowers.push(flower);
+  }
 }
 
 
@@ -28,5 +49,14 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+
+  // Display the grass
+  background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
+
+  // Loop through all the flowers in the array and display them
+  for (let i = 0; i < garden.flowers.length; i++) {
+    let flower = garden.flowers[i];
+    flower.display();
+  }
 
 }
