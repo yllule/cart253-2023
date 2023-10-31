b3e958c9a0f6d029bf7075dd2de953897492345d
yllule <catherinezaloshnja@gmail.com>
2023-10-31 13:08:24 -0400

    Finished object oriented programming activity

--- activities/08-object-oriented-programming-activity/template-p5-project/js/script.js
+++ activities/08-object-oriented-programming-activity/template-p5-project/js/script.js
@@ -1,12 +1,17 @@
 /**
- * Title of Project
- * Author Name
+ * Juggling simulator
+ * Catherine Zaloshnja
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
  */
 
 "use strict";
+
+let gravityForce = 0.0025;
+
+let paddle;
+
+let balls = [];
+let numBalls = 3;
 
 /**
  * Description of preload
@@ -20,6 +25,16 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    paddle = new Paddle(300,20);
+
+    for (let i = 0; i < numBalls; i++) {
+        let x = random(0, width);
+        let y = random(-400, -100);
+        let ball = new Ball(x,y);
+        balls.push(ball);
+    }
 
 }
 
@@ -28,5 +43,19 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
+
+    paddle.move();
+    paddle.display();
+
+    for (let i = 0; i < balls.length; i++) {
+        let ball = balls[i];
+        if (ball.active) {
+            ball.gravity(gravityForce);
+            ball.move();
+            ball.bounce(paddle);
+            ball.display();
+        }
+    }
 
 }
