239a019f2b5aea3d73946ed2f80248a1c909dc2f
yllule <catherinezaloshnja@gmail.com>
2023-11-09 13:55:14 -0500

    Started project 2, created folder

---
+++ project_2/template-p5-project/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Title of Project
+ * Catherine Zaloshnja
+ * Project 2
+ * 
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
