b2a035bdd0445fb3589c38bcd89b1efc987b2eb1
Catherine Z <catherinezaloshnja@gmail.com>
2023-11-15 23:53:29 -0500

    Finished sound activity

--- activities/10-sound-activity/template-p5-project/js/script.js
+++ activities/10-sound-activity/template-p5-project/js/script.js
@@ -6,6 +6,10 @@
 
 "use strict";
 
+let balls = [];
+
+let notes = [`F3`, `G3`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F4`];
+
 /**
  * Description of preload
 */
@@ -18,6 +22,8 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(600,600);
+    //userStart
 
 }
 
@@ -26,5 +32,23 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
 
+    for (let i = 0; i < balls.length; i++) {
+        let ball = balls[i];
+        ball.move();
+        ball.bounce();
+        ball.display();
+    }
+
+}
+
+function mousePressed() {
+    createBall(mouseX, mouseY);
+}
+
+function createBall(x,y) {
+    let note = random(notes);
+    let ball = new Ball(x,y,note);
+    balls.push(ball);
 }
