fd2b2ce50aaa18aeb9056800e8b09dec0b60b02f
yllule <catherinezaloshnja@gmail.com>
2023-11-16 13:44:32 -0500

    Started make some noise exercise

---
+++ exercises/Make-some-noise-exercise/js/script.js
@@ -0,0 +1,32 @@
+/**
+ * Make some noise exercise
+ * Catherine Zaloshnja
+ * 
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+createCanvas(600,600);
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+    background(255, 200, 200);
+
+}
