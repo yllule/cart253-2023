f2acb4ad9ccf57656569c670494c0e1eb1fd61c7
Catherine Z <catherinezaloshnja@gmail.com>
2023-11-15 23:02:40 -0500

    Setting up the docs for the sound activity

---
+++ activities/10-sound-activity/template-p5-project/js/script.js
@@ -0,0 +1,30 @@
+/**
+ * Sound activity
+ * Catherine Zaloshnja
+ * 
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
