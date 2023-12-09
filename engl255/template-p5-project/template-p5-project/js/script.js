e8ca213e349b0fc15b831b69804134d8e03efbf9
Catherine Z <catherinezaloshnja@gmail.com>
2023-12-08 20:46:33 -0500

    added img and position it

--- engl255/template-p5-project/template-p5-project/js/script.js
+++ engl255/template-p5-project/template-p5-project/js/script.js
@@ -1,17 +1,25 @@
 /**
- * Title of Project
- * Author Name
+ * ENGL255 Final project
+ * Catherine Zaloshnja
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * If anyone from CART is reading this, I'm just lazy and using my CART253 repo for my ENGL final project which is an interactive illustration.
  */
 
 "use strict";
+
+let imgPos = {
+    x: 0,
+    y: 0
+}
+
+let illuImg;
 
 /**
  * Description of preload
 */
 function preload() {
+
+    illuImg = loadImage("assets/images/eng_illu.png");
 
 }
 
@@ -20,6 +28,7 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(1000, 750);
 
 }
 
@@ -28,5 +37,17 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
+
+    //start position
+    imageMode(CENTER);
+    image(illuImg, width/2, -250);
+
+    //end position
+    imageMode(CENTER);
+    image(illuImg, width/2, 1000);
 
 }
+
+//notes : make it so the up and down arrows (or mouse scroll?) makes the img scroll
+//add states, title screen with instructions + title
